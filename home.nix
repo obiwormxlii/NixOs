63588a31eae2147a32cda963a44bf5097140065f
obiwormxlii <babrayton0811@gmail.com>
2024-09-02 18:54:00 -0400

    Nix Home Manager

---
+++ home.nix
@@ -0,0 +1,175 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "obi";
+  home.homeDirectory = "/home/obi";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+    pkgs.nixfmt-rfc-style
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #vim.api
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/obi/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.fzf.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "obiwormxlii";
+    userEmail = "babrayton0811@gmail.com";
+  };
+
+  programs.gh = {
+    enable = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+      theme = "jonathan";
+    };
+
+  };
+  programs.neovim =
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
+      enable = true;
+
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+
+      extraPackages = with pkgs; [
+        lua-language-server
+        xclip
+        wl-clipboard
+      ];
+
+      plugins = with pkgs.vimPlugins; [
+
+        nvim-lspconfig
+
+        comment-nvim
+
+        {
+          plugin = gruvbox-nvim;
+          config = "colorscheme gruvbox";
+        }
+
+        neodev-nvim
+
+        nvim-cmp
+
+        telescope-nvim
+
+        telescope-fzf-native-nvim
+
+        cmp_luasnip
+        cmp-nvim-lsp
+
+        luasnip
+        friendly-snippets
+
+        lualine-nvim
+        nvim-web-devicons
+
+        vim-nix
+
+        {
+          plugin = conform-nvim;
+          config = toLuaFile ./nvim/plugin/conform.lua;
+        }
+
+        # {
+        #   plugin = vimPlugins.own-onedark-nvim;
+        #   config = "colorscheme onedark";
+        # }
+      ];
+
+      extraLuaConfig = ''
+        ${builtins.readFile ./nvim/options.lua}
+      '';
+    };
+}
