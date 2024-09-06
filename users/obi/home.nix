4738dd35f82720c8a640992e5f5975fe865a3c0a
obiwormxlii <babrayton0811@gmail.com>
2024-09-06 17:17:58 +0000

    Add Starship

--- users/obi/home.nix
+++ users/obi/home.nix
@@ -22,9 +22,20 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    pkgs.nixfmt-rfc-style
+  home.packages = with pkgs;[
+    nixfmt-rfc-style
+
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Ubuntu" ];})
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+    serif = ["Firacode Nerd Font"];
+    sansSerif = ["JetBrainsMono Nerd Font"];
+    monospace = ["JetBrainsMono Nerd Font"];
+    };
+  };
 
   home.file = {
   };
@@ -48,6 +59,13 @@
   programs.gh = {
     enable = true;
   };
+  
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      eval "$(starship init bash)"
+    '';
+  };
 
   programs.zsh = {
     enable = true;
@@ -59,6 +77,62 @@
     };
 
   };
+
+  programs.starship = {
+  enable = true;
+  # You can add custom settings here if needed
+  settings = {
+    # Your Starship configuration goes here
+    add_newline = false;
+    # Add more configuration options as needed
+    format = "[](fg:#7DF9AA)\[󱄅 ](bg:#7DF9AA fg:#090c0c)\[](fg:#7DF9AA bg:#1C3A5E)\$time\[](fg:#1C3A5E bg:#3B76F0)\$directory\[](fg:#3B76F0 bg:#FCF392)\$git_branch\$git_status\$git_metrics\$character";
+    directory = {
+      format = "[ ﱮ $path ]($style)";
+      style = "fg:#E4E4E4 bg:#3B76F0";
+      };
+
+    git_branch = {
+      format = ''[ $symbol$branch(:$remote_branch) ]($style)'';
+      symbol = "  ";
+      style = "fg:#1C3A5E bg:#FCF392";
+      };
+
+    git_status = {
+        format = ''[$all_status]($style)'';
+        style = "fg:#1C3A5E bg:#FCF392";
+      };
+
+    git_metrics = {
+        format = ''([+$added]($added_style))[ ]($added_style)'';
+        added_style = "fg:#1C3A5E bg:#FCF392";
+        deleted_style = "fg:bright-red bg:235";
+        disabled = false;
+      };
+
+    hg_branch = {
+        format = ''$symbol$branch ]($style)'';
+        symbol = " ";
+      };
+
+    cmd_diration = {
+        format = ''[  $duration ]($style)'';
+        style = "fg:bright-white bg:18";
+      };
+
+    character = {
+        success_symbol = ''[](bold green) '';
+        error_symbol = ''[](#E84D44) '';
+      };
+
+    time = {
+        disabled = false;
+        time_format = "%R"; # Hour:Minute Format
+        style = "bg:#1d2230";
+        format = ''[[ 󱑍 $time ](bg:#1C3A5E fg:#8DFBD2)]($style)'';
+      };
+    };
+  };
+
   programs.neovim =
     let
       toLua = str: "lua << EOF\n${str}\nEOF\n";
