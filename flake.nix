b926a18be8be264fe2a4375e709f809f61b51f16
obiwormxlii <babrayton0811@gmail.com>
2024-09-09 04:46:55 +0000

    Add Tmux

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
         git
         curl
         wget
+        fzf
         ];
     };
 
--- users/obi/home.nix
+++ users/obi/home.nix
@@ -10,6 +10,7 @@
   imports = [
     ./neovim.nix
     ./starship.nix
+    ./tmux.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
