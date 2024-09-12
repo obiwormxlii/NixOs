5807e37af8f7f11df0c763db4773e81a5f9ce8e0
obiwormxlii <babrayton0811@gmail.com>
2024-09-12 07:01:12 -0400

    Add Home desktop profile

--- flake.nix
+++ flake.nix
@@ -15,53 +15,63 @@
     };
 
   };
+  
 
-  outputs = {nixpkgs, home-manager, nixos-wsl, ... }@inputs: {
-
-    nixosConfigurations = {
-
-      nixos-wsl = let
-        username = "obi";
-        specialArgs = {inherit username;};
-
-      in
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          system = "x86_64-linux";
-          modules = [
-            nixos-wsl.nixosModules.default
-            {
-              wsl = {
-                enable = true;
-                defaultUser = "${username}";
-              };
-              nixpkgs.config.allowUnfree = true;
-            }
-
-            home-manager.nixosModules.home-manager {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-
-              home-manager.extraSpecialArgs = inputs // specialArgs;
-              home-manager.users.${username} = ./users/${username}/home.nix;
-            }
+outputs = {nixpkgs, home-manager, nixos-wsl, ... }@inputs: {
+  nixosConfigurations = {
+    nixos-wsl = let
+      username = "obi";
+      specialArgs = {inherit username;};
+    in nixpkgs.lib.nixosSystem {
+      inherit specialArgs;
+      system = "x86_64-linux";
+      modules = [
+        nixos-wsl.nixosModules.default
+        {
+          wsl = {
+            enable = true;
+            defaultUser = "${username}";
+          };
+          nixpkgs.config.allowUnfree = true;
+          nix.settings = {
+            experimental-features = [ "nix-command" "flakes" ];
+            extra-experimental-features = ["nix-command"];
+          };
+          system.stateVersion = "24.05";
+          environment.systemPackages = with nixpkgs; [
+            vim
+            git
+            curl
+            wget
+            fzf
           ];
-        };
-      settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-        extra-experimental-features = ["nix-command"];
-      };
-
-      system.stateVersion = "24.05";
-
-        environment.systemPackages = with nixpkgs;[
-        vim
-        git
-        curl
-        wget
-        fzf
-        ];
+        }
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = inputs // specialArgs;
+          home-manager.users.${username} = ./users/${username}/home.nix;
+        }
+      ];
     };
 
+    desktop = let
+      username = "obi";
+      specialArgs = {inherit username;};
+    in nixpkgs.lib.nixosSystem {
+      inherit specialArgs;
+      system = "x86_64-linux";
+      modules = [
+        ./systems/home-desktop/configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = inputs // specialArgs;
+          home-manager.users.${username} = ./users/${username}/home.nix;
+home-manager.backupFileExtension = "backup";          
+        }
+      ];
+    };
   };
+};
 }
