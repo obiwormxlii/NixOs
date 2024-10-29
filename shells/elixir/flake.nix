20902954de64732ba4a22f8d2ab59e052083fb86
obiwormxlii <babrayton0811@gmail.com>
2024-10-29 17:07:29 +0000

    add Elixir shell flake

---
+++ shells/elixir/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = { url = "github:numtide/flake-utils"; };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+   flake-utils.lib.eachDefaultSystem (system:
+    let
+      inherit (pkgs.lib) optional optionals;
+      pkgs = import nixpkgs { inherit system; };
+
+      elixir = pkgs.beam.packages.erlang.elixir;
+    in
+    with pkgs;
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          elixir
+          elixir_ls
+          glibcLocales
+        ] ++ optional stdenv.isLinux inotify-tools
+          ++ optional stdenv.isDarwin terminal-notifier
+          ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+            CoreFoundation
+            CoreServices
+          ]);
+      };
+    });
+}
